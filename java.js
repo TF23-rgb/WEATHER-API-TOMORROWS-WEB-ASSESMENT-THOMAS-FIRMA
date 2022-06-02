1d9dcede953a35766a3316b2e83ba338d41fc962
Thomas Firma <thomasfirma23@gmail.com>
2022-06-02 07:59:55 -0700

    Initial commit

---
+++ java.js
@@ -0,0 +1,38 @@
+var input = document.querySelector('.input_text');
+
+var main = document.querySelector('#name');
+
+var temp = document.querySelector('.temp');
+
+var desc = document.querySelector('.desc');
+
+var clouds = document.querySelector('.clouds');
+
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=dbb0ef7796f25158231e69276328c00b').then(response => response.json()).then(data => 
+{
+
+  var tempValue = data['main']['temp'];
+
+  var nameValue = data['name'];
+
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+
+  desc.innerHTML = "Forecast - "+descValue;
+
+  temp.innerHTML = "Temperature - "+tempValue;
+  
+  input.value ="";
+
+
+})
+
+.catch(err => alert("Unknown City"));
+
+})
